582592d3fba198adc95b1cfeb390b6a7e4c6fe4a
damyka <damyka@microsoft.com>
2021-06-17 16:15:47 -0700

    added link to github

--- PartyCircuits_MicroBit.py
+++ PartyCircuits_MicroBit.py
@@ -12,7 +12,7 @@
 #
 #  Comments, contributions, suggestions, bug reports, and feature
 #  requests are welcome! For source code and bug reports see:
-#  http://github.com/[TODO github path to Hacking STEM]
+#  https://github.com/microsoft/hackingstem-partycircuits-microbit
 #
 #  Copyright 2018, Jen Fox (AKA jenfoxbot)
 #  Microsoft EDU Workshop - HackingSTEM
